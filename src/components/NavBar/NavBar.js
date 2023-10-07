c0c1ae6906d9403c58a290a81e23f996bd696d6a
gonzab <gonzalo.blancobur@gmail.com>
2023-10-07 17:32:31 -0300

    components

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar =() => {
+    return (
+        <nav>
+            <h3>Ecommers</h3>
+            <div>
+                <button>consolas</button>
+                <button>notebooks</button>
+                <button>parlantes</button>
+            </div>
+        </nav>
+    )
+}
+
+export 
